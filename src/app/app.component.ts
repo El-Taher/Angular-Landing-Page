af5e2ec7aaaff12e82aa4a0645815a15629a1286
Ahmed Eltaher <ahmedeltaher64@gmail.com>
2024-11-27 17:25:30 +0200

    Add section3 component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { Section1Component } from './section-1/section-1.component';
 import { Section2Component } from './section-2/section-2.component';
+import { Section3Component } from './section-3/section-3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, Section1Component, Section2Component],
+  imports: [
+    HeaderComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
